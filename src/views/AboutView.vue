ba52f169419347507f3455f227cb71d0072b1ddf
ervinosmani3@gmail.com <ervinosmani3@gmail.com>
2023-03-11 17:47:46 +0100

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,63 @@
 <template>
   <main class="about">
-    <h1>About</h1>
+
+    <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="img-fluid w-100 h-50 overflow-hidden" src="./cover3.png" alt="...">
+            </div>
+    </div>
+    <section id="about" class="about mt-5">
+            <div class="container-fluid">
+                <h2 class="h1-responsive font-weight-bold text-center my-2">About</h2>
+                <!--Section description-->
+                <p class="text-center w-responsive mx-auto mb-1">Do you have any questions? Please do not hesitate to
+                    contact us directly. Our team will come back to you within
+                    a matter of hours to help you.</p>
+                <div class="row  pt-5 pb-5">
+
+                    <div class="col-lg-5 align-items-stretch video-box"
+                        style='background-image: url("h");'>
+                        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4"
+                            data-vbtype="video" data-autoplay="true"></a>
+                    </div>
+
+                    <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch">
+
+                        <div class="content">
+                            <h3>Eum ipsam laborum deleniti <strong>velit pariatur architecto aut nihil</strong></h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                            </p>
+                            <p class="font-italic">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore
+                                magna aliqua.
+                            </p>
+                            <ul>
+                                <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat.</li>
+                                <li><i class="bx bx-check-double"></i> Duis aute irure dolor in reprehenderit in
+                                    voluptate velit.</li>
+                                <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute irure dolor in reprehenderit in voluptate trideta
+                                    storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                            </ul>
+                            <p>
+                                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate
+                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in
+                                culpa qui officia deserunt mollit anim id est laborum
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
   </main>
 </template>
 
