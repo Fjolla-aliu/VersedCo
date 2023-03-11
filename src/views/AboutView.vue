d1b03357d370bae15c19fb0e3ee7caa2679b3740
ervinosmani3@gmail.com <ervinosmani3@gmail.com>
2023-03-11 18:52:54 +0100

    finished about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,6 @@
+
 <template>
+  <br>
   <main class="about">
 
     <div class="carousel-inner">
@@ -6,17 +8,20 @@
                 <img class="img-fluid w-100 h-50 overflow-hidden" src="./img/cover3.png" alt="...">
             </div>
     </div>
-    <section id="about" class="about mt-5">
+ <br><br><br>
+
+    <section id="about" class="about mt-8">
             <div class="container-fluid">
                 <h2 class="h1-responsive font-weight-bold text-center my-2">About</h2>
                 <!--Section description-->
                 <p class="text-center w-responsive mx-auto mb-1">Do you have any questions? Please do not hesitate to
                     contact us directly. Our team will come back to you within
                     a matter of hours to help you.</p>
-                <div class="row  pt-5 pb-5">
+                    <br>
+                <div class="row  pt-8 pb-8 m-4">
 
-                    <div class="col-lg-5 align-items-stretch video-box"
-                        style='background-image: url("h");'>
+                    <div class="col-lg-4 align-items-stretch video-box "
+                        style='background-image: url("https://cdn.shopify.com/s/files/1/0130/2514/8987/articles/blog_tranexamicacid_878ffcce-1a48-4185-b26c-3228cbd6c048_600x.jpg?v=1631907696");'>
                         <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4"
                             data-vbtype="video" data-autoplay="true"></a>
                     </div>
@@ -26,13 +31,10 @@
                         <div class="content">
                             <h3>Eum ipsam laborum deleniti <strong>velit pariatur architecto aut nihil</strong></h3>
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                              Co-created with over 85,342 of you (and counting), our mission is simple:
                             </p>
                             <p class="font-italic">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore
-                                magna aliqua.
+                              MAKE PRODUCTS THAT PERFORM FOR YOUR SKIN, BODY, AND PLANET
                             </p>
                             <ul>
                                 <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo
@@ -44,21 +46,120 @@
                                     storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
                             </ul>
                             <p>
-                                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                reprehenderit in voluptate
-                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                proident, sunt in
-                                culpa qui officia deserunt mollit anim id est laborum
+                              Our products are made with proven ingredients at skin-changing levels, and without the 
+                              fussy packaging and conventional retail markups. With everything under $25, you only pay 
+                              for what matters—the goop inside the bottle.We’re the first skincare brand that’s community
+                               co-created, so you could say listening is in our DNA. From concepting to testing, every product
+                                we make is approved by you.Everything we do has an impact on our planet, which is why we’ve
+                                 maintained net-zero emissions since our launch in 2019. See what else we’re doing to take 
+                                 responsibility for our impact.We believe in transparency at every level. We never airbrush 
+                                 skin and are always upfront about where we're at and where we're headed (just check out our 
+                                 biannual reports and community board meetings).We formulate to the highest standards in the 
+                                 world, European Union standards, nixing 1,350+ ingredients that have questionable data when 
+                                 it comes to the health of our skin, body, and planet.Every ingredient has a purpose, and unless 
+                                 that purpose is proven safe and effective, we skip it. If that ever changes, so will our ‘no thanks’ list. 
                             </p>
                         </div>
 
                     </div>
 
-                </div>
-
-            </div>
+                  </div>
+            </div>    
         </section>
+      
   </main>
+ <br><br><br>
+  <!-- Footer -->
+  <footer class="text-center text-lg-start bg-dark py-3 text-white">
+          
+          <!-- Section: Social media -->
+
+          <!-- Section: Links  -->
+          <section class="">
+              <div class="container text-center text-md-start mt-5">
+                  <!-- Grid row -->
+                  <div class="row mt-3">
+                      <!-- Grid column -->
+                      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                          <!-- Content -->
+                          <h2 class="text-uppercase fw-bold mb-4">
+                              <i class="bi bi-gem me-3"></i>VersedCo
+                          </h2>
+                          <p>
+                              Here you can use rows and columns to organize your footer content. Lorem ipsum
+                              dolor sit amet, consectetur adipisicing elit.
+                          </p>
+                      </div>
+                      <!-- Grid column -->
+
+                      <!-- Grid column -->
+                      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                          <!-- Links -->
+                          <h6 class="text-uppercase fw-bold mb-4">
+                              Products
+                          </h6>
+                          <p>
+                              <a href="#!" class="text-reset">By Corner</a>
+                          </p>
+                          <p>
+                              <a href="#!" class="text-reset">By Category</a>
+                          </p>
+                          <p>
+                              <a href="#!" class="text-reset">By Ingredient</a>
+                          </p>
+                      </div>
+                      <!-- Grid column -->
+
+                      <!-- Grid column -->
+                      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                          <!-- Links -->
+                          <h6 class="text-uppercase fw-bold mb-4">
+                              Useful links
+                          </h6>
+                          <p>
+                              <a href="#!" class="text-reset">Pricing</a>
+                          </p>
+                          <p>
+                              <a href="#!" class="text-reset">Settings</a>
+                          </p>
+                          <p>
+                              <a href="#!" class="text-reset">Orders</a>
+                          </p>
+                          <p>
+                              <a href="#!" class="text-reset">Help</a>
+                          </p>
+                      </div>
+                      <!-- Grid column -->
+
+                      <!-- Grid column -->
+                      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                          <!-- Links -->
+                          <h6 class="text-uppercase fw-bold mb-4">
+                              Contact
+                          </h6>
+                          <p><i class="bi bi-location me-3"></i> New York, NY 10012, US</p>
+                          <p>
+                              <i class="bi bi-envelope me-3"></i>
+                              info@example.com
+                          </p>
+                          <p><i class="bi bi-phone me-3"></i> + 01 234 567 88</p>
+                          <p><i class="bi bi-print me-3"></i> + 01 234 567 89</p>
+                      </div>
+                      <!-- Grid column -->
+                  </div>
+                  <!-- Grid row -->
+              </div>
+          </section>
+
+          <!-- Section: Links  -->
+
+          <!-- Copyright -->
+          <div class="text-center py-4" style="background-color: rgba(0, 0, 0, 0.05);">
+              © 2021 Copyright:
+              <a class="text-reset fw-bold" href="/">VersedCo.com</a>
+          </div>
+          <!-- Copyright -->
+      </footer>
 </template>
 
 
